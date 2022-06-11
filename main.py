d564b6f8d512f47a2028d5fcd24419d37f6edd41
TOMOKI <hhsgwtmk@gmail.com>
2022-06-12 01:07:20 +0900

    1st commit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-#from multiprocessing import Condition
-#from turtle import width
 import streamlit as st
 import numpy as np
 import pandas as pd
-#from PIL import Image
 import time
 
 
@@ -29,29 +26,3 @@ if button:
 
 expander = st.expander('問い合わせ')
 expander.write('問い合わせ内容を書く')
-# text = st.text_input('あなたの趣味を教えてください。')
-# condition = st.slider('あなたの今の調子は？', 0, 100, 50)
-# 'あなたの趣味:', text, 'です。'
-# 'コンディション:', condition
-# if st.checkbox('show imag'):
-#     img = Image.open('みか.jpg')
-#     st.image(img, caption='mika hasegawa',use_column_width=True)
-# df=pd.DataFrame(
-#     np.random.rand(100, 2)/[50, 50]+[35.69, 139.70],
-#     columns=['lat', 'lon']
-# )
-# st.map(df)
-#st.table(df.style.highlight_max(axis=0))
-#.style.highlight_max(axis=0)
-#, width=100, height=100
-#dataframe(df.style.highlight_max(axis=0), width=100, height=100)
-# """
-# # 章項
-# ## 節
-# ### 項
-# ```python
-# import streamlit as st
-# import numpy as np
-# import pandas as pd
-# ```
-# """
